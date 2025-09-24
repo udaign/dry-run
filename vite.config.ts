0f9024516fe87f9c1465838f41fae75cc9bc25a7
Udaya Bhaskar Mahadevu <udaybhaskar2283@gmail.com>
2025-09-24 12:35:38 +0530

    Clipboard Functionality

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,10 @@ export default defineConfig(({ mode }) => {
 
             base : '/dry-run/',
       
+      server: {
+        port: 3000,
+        host: '0.0.0.0',
+      },
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
