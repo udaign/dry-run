1fbd0dde5416065ae3145811e4e7734bd4dd7891
Udaya Bhaskar Mahadevu <udaybhaskar2283@gmail.com>
2025-11-20 01:25:33 +0530

    Install - App

--- App.tsx
+++ App.tsx
@@ -584,7 +584,7 @@ const App: React.FC = () => {
           <div className="flex items-center space-x-2">
             {isMobile && installPrompt && (
               <button onClick={handleInstallClick} className={baseButtonClasses} aria-label="Install app">
-                <span>Install</span>
+                <span>App</span>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-6 w-6 ml-2">
                   <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
                   <polyline points="7 10 12 15 17 10"></polyline>
